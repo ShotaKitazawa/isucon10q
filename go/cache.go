4a5ce6fb93f5b6fd952dad67b518b0674836359b
ranksai <ranks.j.inter1908@gmail.com>
2020-09-12 19:24:32 +0900

    fix

--- go/cache.go
+++ go/cache.go
@@ -12,18 +12,26 @@ var chairLock sync.Mutex
 func initCache() {
 	estateMap = make(map[int64]Estate, 100000)
 	chairMap = make(map[int64]Chair, 100000)
-
+	estateStructs = []Estate{}
 	if err := db.Select(&estateStructs, `SELECT * FROM estate`); err != nil {
 		panic(err)
 	}
+	chairStructs = []Chair{}
 	if err := db.Select(&chairStructs, `SELECT * FROM chair`); err != nil {
 		panic(err)
 	}
-
 	for _, estate := range estateStructs {
 		estateMap[estate.ID] = estate
 	}
 	for _, chair := range chairMap {
 		chairMap[chair.ID] = chair
 	}
+}
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
