02a9ceb0bb7311746664dd509ab30ab0ea0568a3
ShotaKitazawa <skitazawa1121@gmail.com>
2020-09-12 16:10:27 +0900

    update

--- go/estate.go
+++ go/estate.go
@@ -5,7 +5,7 @@ import "github.com/k0kubun/pp"
 var estateStructs []Estate
 
 func initEstate() {
-	if err := db.Select(estateStructs, `SELECT * FROM estate`); err != nil {
+	if err := db.Select(&estateStructs, `SELECT * FROM estate`); err != nil {
 		panic(err)
 	}
 	pp.Print(estateStructs)
