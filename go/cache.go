d102b40c7bdf654c67bd2df347bc260f2dab3f08
ShotaKitazawa <skitazawa1121@gmail.com>
2020-09-17 00:20:21 +0900

    update

--- go/cache.go
+++ go/cache.go
@@ -6,14 +6,9 @@ import (
 	"strconv"
 )
 
-//var estateMap map[int64]Estate
-//var estateLock sync.Mutex
-//var chairMap map[int64]Chair
-//var chairLock sync.Mutex
-
 func initCache() {
-	// estateMap = make(map[int64]Estate, 100000)
-	// chairMap = make(map[int64]Chair, 100000)
+
+	rdb.FlushAll(context.Background())
 
 	var estateStructs []Estate
 	var chairStructs []Chair
