1e452eed40d3ac11e754d182af917c4fbe646b4f
ShotaKitazawa <skitazawa1121@gmail.com>
2020-09-17 01:17:50 +0900

    update

--- go/cache.go
+++ go/cache.go
@@ -6,17 +6,49 @@ import (
 	"strconv"
 )
 
+type ChairForCache struct {
+	ID              int64  `db:"id" json:"id"`
+	Name            string `db:"name" json:"name"`
+	Description     string `db:"description" json:"description"`
+	Thumbnail       string `db:"thumbnail" json:"thumbnail"`
+	Price           int64  `db:"price" json:"price"`
+	Height          int64  `db:"height" json:"height"`
+	Width           int64  `db:"width" json:"width"`
+	Depth           int64  `db:"depth" json:"depth"`
+	Color           string `db:"color" json:"color"`
+	Features        string `db:"features" json:"features"`
+	Kind            string `db:"kind" json:"kind"`
+	Popularity      int64  `db:"popularity" json:"popularity"`
+	Stock           int64  `db:"stock" json:"stock"`
+	PopularityMinus int64  `db:"popularity_minus" json:"-"`
+}
+
+type EstateForCache struct {
+	ID              int64   `db:"id" json:"id"`
+	Thumbnail       string  `db:"thumbnail" json:"thumbnail"`
+	Name            string  `db:"name" json:"name"`
+	Description     string  `db:"description" json:"description"`
+	Latitude        float64 `db:"latitude" json:"latitude"`
+	Longitude       float64 `db:"longitude" json:"longitude"`
+	Address         string  `db:"address" json:"address"`
+	Rent            int64   `db:"rent" json:"rent"`
+	DoorHeight      int64   `db:"door_height" json:"doorHeight"`
+	DoorWidth       int64   `db:"door_width" json:"doorWidth"`
+	Features        string  `db:"features" json:"features"`
+	Popularity      int64   `db:"popularity" json:"popularity"`
+	PopularityMinus int64   `db:"popularity_minus" json:"-"`
+}
+
 func initCache() {
 
 	rdb.FlushAll(context.Background())
 
-	var estateStructs []Estate
-	var chairStructs []Chair
+	var estateStructs []EstateForCache
+	var chairStructs []ChairForCache
 
 	if err := db.Select(&estateStructs, `SELECT * FROM estate`); err != nil {
 		panic(err)
 	}
-	chairStructs = []Chair{}
 	if err := db.Select(&chairStructs, `SELECT * FROM chair`); err != nil {
 		panic(err)
 	}
